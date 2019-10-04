cf6bf07ebd53af8c5edeaa67cc79a90b7f37731b
Vassil Kateliev <vassil@kateliev.com>
2019-10-04 12:41:37 +0300

    Init

---
+++ lib/__init__.py
@@ -0,0 +1,11 @@
+# MODULE: DeltaMachine | Typerig
+# ----------------------------------------
+# (C) Vassil Kateliev, 2018 (http://www.kateliev.com)
+# (C) Karandash Type Foundry (http://www.karandash.eu)
+#-----------------------------------------
+# www.typerig.com 
+
+# No warranties. By using this you agree
+# that you use it at your own risk!
+
+__version__ = '0.2.5'
